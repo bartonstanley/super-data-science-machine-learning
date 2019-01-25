4720fb7b70a49fcc7ae56dfb4301e096c0ec66d1
Barton Stanley <bartonstanley@gmail.com>
2019-01-24 20:16:01 -0600

    lesson 52

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -20,16 +20,34 @@ test_set = subset(dataset, split == FALSE)
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Multiple Linear Regression to the Training set
+# Fitting Multiple Linear Regression to the Training set.
 regressor = lm(formula = Profit ~ .,
                data = training_set)
 
 # Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
 
-# Fitting Multiple Linear Regression to the Training set using only R&D spending
-regressor = lm(formula = Profit ~ R.D.Spend,
-               data = training_set)
+# Fitting Multiple Linear Regression to the Training set, backward eliminiation
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
 
-# Predicting the Test set results
-y_pred = predict(regressor, newdata = test_set)
+# Fitting Multiple Linear Regression to the Training set, remove State because
+# State2 p-value = 0.990 and State3 p-value = 0.943, the two highest p-values
+# and both are greater than 0.05.
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Fitting Multiple Linear Regression to the Training set, remove Administration because
+# Administration p-value = 0.602, now the highest p-value and is greater than 0.05.
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Fitting Multiple Linear Regression to the Training set, remove Marketing.Spend because
+# Marketing.Spend p-value = 0.06, now the highest p-value and is greater than 0.05.
+# Note, however that it is close to 0.05 so there may be reasons to leave it in.
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
