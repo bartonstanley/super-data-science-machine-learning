0531352afbef1cad085b0255d1f1ba1f429181db
Barton Stanley <bartonstanley@gmail.com>
2019-01-30 20:04:29 -0600

    updated auto backward elimination using step function

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/auto_backward_elimination.R
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/auto_backward_elimination.R
@@ -1,24 +1,7 @@
-backwardElimination <- function(x, sl) {
-  numRows = length(x) + 1
-  for (i in c(1:numVars)){
-    print(x)
-    regressor = lm(formula = Profit ~ ., data = x)
-    maxVar = max(coef(summary(regressor))[c(2:numRows), "Pr(>|t|)"])
-    print(maxVar)
-    if (maxVar > sl){
-      j = which(coef(summary(regressor))[c(2:numRows), "Pr(>|t|)"] == maxVar)
-      x = x[, -j]
-      print(x)
-    }
-    numRows = numRows - 1
-  }
-  return(summary(regressor))
-}
-
 dataset = read.csv('50_Startups.csv')
 dataset$State = factor(dataset$State,
                          levels = c('Florida', 'New York', 'California'),
                          labels = c(1, 2, 3))
 regressor = lm(formula = Profit ~ ., data = dataset)
 
-backwardElimination(dataset, 0.05)
+summary(step(regressor))
