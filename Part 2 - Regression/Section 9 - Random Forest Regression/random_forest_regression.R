115549d2292af7000346f9c9d97fc9b1504e4d69
Barton Stanley <bs069889.cerner.net>
2019-02-18 14:55:25 -0600

    added random forest regression folder and deleted unneeded files

--- Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.R
+++ Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.R
@@ -1,4 +1,4 @@
-# Regression Template
+# Random Forest Regression
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -16,33 +16,26 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting Random Forest Regression to the dataset
+# install.packages('randomForest')
+library(randomForest)
+set.seed(1234)
+regressor = randomForest(x = dataset[-2],
+                         y = dataset$Salary,
+                         ntree = 500)
 
-# Predicting a new result
+# Predicting a new result with Random Forest Regression
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
+# Visualising the Random Forest Regression results (higher resolution)
 # install.packages('ggplot2')
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Random Forest Regression)') +
   xlab('Level') +
   ylab('Salary')
