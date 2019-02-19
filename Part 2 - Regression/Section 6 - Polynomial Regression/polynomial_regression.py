fc4616dd6173233c0f1f209fb1dc8d39038ac779
Barton Stanley <bartonstanley@gmail.com>
2019-02-18 19:54:41 -0600

    use loc instead of iloc

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -7,8 +7,11 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+
+# Use .loc instead of .iloc to make it clearer which columns we are using
+X = dataset.loc[:, 'Level':'Level'].values
+X.resize(X.size, 1)
+y = dataset.loc[:, 'Salary'].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -60,7 +63,11 @@ plt.ylabel('Salary')
 plt.show()
 
 # Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
+# Had to change this to pass a 2D array instead of a scalar due to error message
+# was:  was: lin_reg.predict(np.array(6.5))
+lin_reg.predict(np.array(6.5).reshape(-1, 1))
 
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
+# Had to change this to pass a 2D array instead of a scalar due to error message
+# was: lin_reg_2.predict(poly_reg.fit_transform(np.array(6.5))
+lin_reg_2.predict(poly_reg.fit_transform(np.array(6.5).reshape(-1, 1)))
